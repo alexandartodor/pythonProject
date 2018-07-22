8eeb2fd7ee1a5cf3cfe18d3ac6b9c7c1cbce22ed
alexandartodor <32853561+alexandartodor@users.noreply.github.com>
2018-07-22 12:04:03 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,8 @@
+
+color = input("Enter a color: ")
+pluralnoun = input("Enter a Plural noun: ")
+celebrity = input("Celebrity: ")
+
+print("Roses are " + color)
+print(pluralnoun + " are blue")
+print("I love " + celebrity)
