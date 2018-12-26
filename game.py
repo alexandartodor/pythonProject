ef6dbab8504f6f521d75e95ee3f1a01fbc969310
Aleksandar <alexandartodor@gmail.com>
2018-12-26 15:17:04 +0100

    push to branch01

--- game.py
+++ game.py
@@ -3,6 +3,6 @@ color = input("Enter a color: ")
 pluralnoun = input("Enter a Plural noun: ")
 celebrity = input("Celebrity: ")
 
-print("Roses are " + col)
+print("Roses are " + colpr)
 print(pluralnoun + " are blue")
 print("I love " + celebrity)
